424ac73cb6268c6165d76ad4e78157e40683fe77
linbin <LanguageBind@users.noreply.huggingface.co>
2024-04-07 05:30:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,2 @@
 import os
-os.system('pip install -e .')
-import time
-print(111111111)
-time.sleep(3)
-print(222222222)
-os.system('python  opensora/serve/gradio_web_server.py')
+os.system('python -m opensora/serve/gradio_web_server')
