05dac610fd9c88d1e47b3a9c04ca4cabded0d284
linbin <LanguageBind@users.noreply.huggingface.co>
2024-04-07 06:36:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import os
-
+import sys
+current_path = os.path.abspath(__file__)
+parent_path = os.path.dirname(current_path)
+print(parent_path)
+sys.path.append(parent_path)
+print(sys.path)
 os.system('python opensora/serve/gradio_web_server.py')
